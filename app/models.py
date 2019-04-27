6d6fb15e81a712643fb0da9aee925513335125cf
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-27 12:46:52 +0300

    corrections made

--- app/models.py
+++ app/models.py
@@ -75,7 +75,7 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    pitch_id = db.Colimn(db.integer)
+    pitch_id = db.Column(db.Integer)
     comment = db.Column(db.String(1000))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
