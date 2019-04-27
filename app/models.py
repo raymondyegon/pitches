14594ebfb0f8a52eb1f43022fc372a00b8b2c979
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-27 10:32:34 +0300

    Methods to get pitches by id and category added

--- app/models.py
+++ app/models.py
@@ -50,15 +50,22 @@ class Pitch(db.Model):
     category = db.Column(db.String)
     posted = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    
-    #Saving a pitch 
+
+    # Saving a pitch
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
-    
+
     # getting pitches by category
     @classmethod
     def get_pitches(cls, category):
-        pitches = Pitch.query.filter_by(category = category).all()
-        
+        pitches = Pitch.query.filter_by(category=category).all()
+
         return pitches
+
+    # gettinga single pitch by id
+    @classmethod
+    def get_pitch(cls, id):
+        pitch = Pitch.query.filter_by(id=id).first()
+
+        return pitch
