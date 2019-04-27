226fb66a4d8b51f07f0ff9e7f33a804a80fdb396
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-27 21:28:45 +0300

    db command added

--- manage.py
+++ manage.py
@@ -1,13 +1,17 @@
 from app import create_app, db
 from app.models import User, Pitch, Comment, PhotoProfile
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
 # creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
 
 
 @manager.command
